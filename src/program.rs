73f3344e8d7a52136c66ddf210cc93a9065b3120
Overpeek <overpeek.fin@gmail.com>
2022-04-20 14:58:35 +0300

    multiple default programs

--- src/program.rs
+++ src/program.rs
@@ -44,7 +44,51 @@ impl DefaultVertex {
 
 glium::implement_vertex!(DefaultVertex, vi_position, vi_color, vi_uv);
 
-pub fn default_program<F>(facade: &F) -> Program
+/// Takes in `DefaultVertex` VBO
+///
+/// Takes in an UBO with "mat" -> Mat4x4 MVP matrix
+pub fn color_2d_program<F>(facade: &F) -> Program
+where
+    F: Facade,
+{
+    glium::program!(facade,
+        140 => {
+            vertex: "#version 140
+                in vec2 vi_position;
+                in vec4 vi_color;
+                in vec2 vi_uv;
+
+                uniform mat4 mat;
+
+                out vec4 fi_color;
+                out vec2 fi_uv;
+
+                void main() {
+                    gl_Position = mat * vec4(vi_position, 0.0, 1.0) * vec4(1.0, -1.0, 1.0, 1.0);
+                    fi_color = vi_color;
+                    fi_uv = vi_uv;
+                }",
+            fragment: "#version 140
+                in vec4 fi_color;
+                in vec2 fi_uv;
+
+                out vec4 o_color;
+
+                void main() {
+                    o_color = fi_color;
+                }",
+            outputs_srgb: true
+        }
+    )
+    .unwrap_or_else(|err| panic!("Default program failed to compile: {}", err))
+}
+
+/// Takes in `DefaultVertex` VBO
+///
+/// Takes in an UBO with "mat" -> Mat4x4 MVP matrix
+///
+/// Takes in texture2D sampler
+pub fn texture_2d_program<F>(facade: &F) -> Program
 where
     F: Facade,
 {
